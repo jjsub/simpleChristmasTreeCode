34ccc63028ac59c044948cf6330efd9d2392134d
jjsub <jsamesub@gmail.com>
2015-03-30 13:23:44 -0500

    tree base

---
+++ treeAndBase.js
@@ -0,0 +1,34 @@
+function christmasTree(height) {
+  var star = "*"; // number of stars for the top of tree
+  var space = " ";
+  var output = "";
+
+  for (var i = height; i > 0; i--) {
+
+    // add spaces needed for each row
+    for (var y = i; y > 0; y--) {
+      output += space;
+    }
+    output += star;
+    console.log(output);
+    star += "**";
+    output = ""; // reset output string for each row
+  }
+}
+
+christmasTree(25);
+
+// it needs jevy refactoring 
+
+/*=========== 
+  Tree Base
+============*/
+
+var espa = "";
+var spe = " ";
+
+for( p = 25; p > 0; p--){
+     espa += spe;
+  }
+
+console.info(espa + "*");
